309ab68c7d6c9a07129e3c2db73c649a256fb680
lphiewpenco <69580901+lphiewpenco@users.noreply.github.com>
2023-04-05 00:30:37 +0800

    Update State_Machine_Robot.ino
    Modify For Proper State Machine Implementation

--- State_Machine_Robot/State_Machine_Robot.ino
+++ State_Machine_Robot/State_Machine_Robot.ino
@@ -1,211 +1,296 @@
-//Locomotion
-#define wheel1 2 //upper left
-#define wheel2 3 //upper right
-#define wheel3 4 //lower left
-#define wheel4 5 //lower right
-#define dir_1 0 //upper left direction
-#define dir_2 1 //upper right direction
-#define dir_3 2 //lower left direction
-#define dir_4 3 //lower right direction
-#define motor_speed 120
 
 //PS4
 #include <PS4BT.h>
 #include <usbhub.h>
-
+#include <TimedAction.h>
 // Satisfy the IDE, which needs to see the include statment in the ino too.
 #ifdef dobogusinclude
 #include <spi4teensy3.h>
 #endif
 #include <SPI.h>
+#include "ProgramConstant.h"
+#include "Variable.h"
 //USB Set UP
-USB Usb;
-USBHub Hub1(&Usb); // Some dongles have a hub inside
-BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
 
-/* You can create the instance of the PS4BT class in two ways */
-// This will start an inquiry and then pair with the PS4 controller - you only have to do this once
-// You will need to hold down the PS and Share button at the same time, the PS4 controller will then start to blink rapidly indicating that it is in pairing mode
-PS4BT PS4(&Btd, PAIR);
 
-// input
-int input_signal = 0;
 
-enum 
+void DebugMessageTaskCode()
 {
-  NoPress,
-  Up,
-  Down,
-  Left,
-  Right,
-  Square,
-  Circle,
-  Cross,
-  Triangle
-} button;
-
-enum 
-{
-  input,
-  process,
-  output
-} progress;
-
-enum
-{
-  initialisation,
-  start,
-  rectanglemove
-} state;
-
-//Locomotion
-void Forward(int pwm)
-{
-    PORTA |= (1 << dir_1);
-    PORTA &= ~(1 << dir_2);
-    PORTA |= (1 << dir_3);
-    PORTA &= ~(1 << dir_4);
-      
-    analogWrite(wheel1, pwm);
-    analogWrite(wheel2, pwm);
-    analogWrite(wheel3, pwm);
-    analogWrite(wheel4, pwm);
+  if(DebugMessage != "")
+  {
+    Serial.print(DebugMessage);
+    DebugMessage = "";          // Clear away the message so that serial terminal wont full of repeated useless message
+  }
 }
 
-void Backward(int  pwm)
+
+void PS4_Repeat_Init_Code()
+{
+  if (Usb.Init() == -1)
+  {
+    USB_Detected = false;
+    DebugMessage = F("USB host init fail");
+    PS4_Repeat_Init_Task.enable();          // It is ok to keep enable, it is just a flag only, not starting another thread
+  }
+  else
+  {
+    USB_Detected = true;
+    DebugMessage =F("\r\nPS4 Bluetooth Library Started");
+    PS4_Repeat_Init_Task.disable();         // Disable PS4_Repeat_Init_Task, it means PS4_Repeat_Init_Task.check() will basically do nothing
+  }
+}
+void InputTaskCode()
+{
+  UP_Pressed        = PS4.getButtonPress(UP)?        true:UP_Pressed;
+  RIGHT_Pressed     = PS4.getButtonPress(RIGHT)?     true:RIGHT_Pressed;
+  DOWN_Pressed      = PS4.getButtonPress(DOWN)?      true:DOWN_Pressed;
+  LEFT_Pressed      = PS4.getButtonPress(LEFT)?      true:LEFT_Pressed;
+  L1_Pressed        = PS4.getButtonPress(L1)?        true:L1_Pressed;
+  SQUARE_Pressed    = PS4.getButtonPress(SQUARE)?    true:SQUARE_Pressed;
+  CIRCLE_Pressed    = PS4.getButtonPress(CIRCLE)?    true:CIRCLE_Pressed;
+  TRIANGLE_Pressed  = PS4.getButtonPress(TRIANGLE)?  true:TRIANGLE_Pressed;
+  CROSS_Pressed     = PS4.getButtonPress(CROSS)?     true:CROSS_Pressed;
+  START_Pressed   =   PS4.getButtonPress(OPTIONS)?   true:START_Pressed;
+  // Scan IMU Reading from serial
+  // Scan ON/OFF Switch or Sensor Reading
+}
+
+void ClearButtonStatus()
+{
+  UP_Pressed        = false;
+  RIGHT_Pressed     = false;
+  DOWN_Pressed      = false;
+  LEFT_Pressed      = false;
+  L1_Pressed        = false;
+  SQUARE_Pressed    = false;
+  CIRCLE_Pressed    = false;
+  TRIANGLE_Pressed  = false;
+  CROSS_Pressed     = false;
+  START_Pressed   =   false;  
+}
+void ProcessTaskCode()
+{
+  switch(OperatingState)
+  {
+    case USB_Detect_Hold:
+        if (USB_Detected = true) OperatingState = WaitStart; // Set First State to wait user press Start button
+        else DebugMessage = F("Detecting PS4 from USB Host ");
+      break;
+    case WaitStart:
+      if(START_Pressed)
+        OperatingState = CheckActionToDo ;
+        ClearButtonStatus(); // May be "START_Pressed = false" is more relevan
+      break;
+        
+    case CheckActionToDo:
+      // Please Take note that the following code process Button in priority manner
+      if(CIRCLE_Pressed)
+      {
+        OperatingState = CircleMove;
+        Serial.println("User Select to Move In Circle");
+      }
+      else if (SQUARE_Pressed)
+      {
+        OperatingState = RectangleMove;
+        Serial.println("User Select to Move In Rectangle");
+      }
+      else if (UP_Pressed)
+      {
+        OperatingState = Move_Forward;
+        Serial.println("User Select to Move Forward");
+      }
+      else if (DOWN_Pressed)
+      {
+        OperatingState = Move_Backward;
+        Serial.println("User Select to Move Backward");
+      }
+      else if (LEFT_Pressed)
+      {
+        OperatingState = Move_Left;
+        Serial.println("User Select to Move Left");
+      }
+      else if (RIGHT_Pressed)
+      {
+        OperatingState = Move_Right;
+        Serial.println("User Select to Move Right");
+      }
+      else 
+      {
+        ClearButtonStatus(); // In Action Checking Cycle, disregard some functional button
+        Serial.println("User Select Non Action Button, Do nothing");
+      }
+      break;
+      
+    case CircleMove:
+      if(CROSS_Pressed) 
+      {
+        // Exit Circle Move Operation 
+        OperatingState = CheckActionToDo;
+      }
+      ClearButtonStatus();      // Check Carefully if this is necessary
+      break;
+      
+    case RectangleMove:
+      if(CROSS_Pressed) 
+      {
+        // Exit Rectangle Move Operation 
+        OperatingState = CheckActionToDo;
+      }
+      ClearButtonStatus();      // Check Carefully if this is necessary
+      break;
+      
+    case Move_Forward:
+      if(CROSS_Pressed) 
+      {
+        // Exit Forward Move Operation 
+        OperatingState = CheckActionToDo;
+      }
+      ForwardSpeed = 100;   // Can actually perform ramping by using counter
+      BackwardSpeed =-1;
+      ClearButtonStatus();      // Check Carefully if this is necessary
+      break;
+      
+    case Move_Backward:
+      if(CROSS_Pressed) 
+      {
+        // Exit Backward Move Operation 
+        OperatingState = CheckActionToDo;
+      }
+      BackwardSpeed = 100;   // Can actually perform ramping by using counter
+      ForwardSpeed =-1;
+      ClearButtonStatus();      // Check Carefully if this is necessary
+      break;
+      
+    case Move_Left:
+      if(CROSS_Pressed) 
+      {
+        // Exit Left Move Operation 
+        OperatingState = CheckActionToDo;
+      }      
+      LeftSpeed = 100;   // Can actually perform ramping by using counter
+      RightSpeed =-1;
+      ClearButtonStatus();      // Check Carefully if this is necessary
+      break;
+      
+    case Move_Right:
+      if(CROSS_Pressed) 
+      {
+        // Exit Right Move Operation 
+        OperatingState = CheckActionToDo;
+      }
+      RightSpeed = 100;   // Can actually perform ramping by using counter
+      LeftSpeed =-1;
+      ClearButtonStatus();      // Check Carefully if this is necessary
+      break;
+      
+    default:                    // For in case something go wrong, force user press Start Button again
+      OperatingState = WaitStart;
+      Serial.println("Warning : Serious Error Occur, Entering Unexpected state");
+      ClearButtonStatus();      // Check Carefully if this is necessary
+  }
+}
+
+void OutputTaskCode()
+{
+  if(LeftSpeed > 0) Leftward(LeftSpeed);
+  else if(RightSpeed > 0) MoveRight(RightSpeed); 
+  else if(ForwardSpeed > 0) MoveFront(ForwardSpeed);
+  else if( BackwardSpeed > 0) MoveBack(BackwardSpeed);
+}
+
+void SetPWM(int PWM_Value)
+{
+  analogWrite(wheel1, PWM_Value);
+  analogWrite(wheel2, PWM_Value);
+  analogWrite(wheel3, PWM_Value);
+  analogWrite(wheel4, PWM_Value);  
+}
+
+//Locomotion
+void MoveFront(int pwm)
+{
+  PORTA &= ~(1 << dir_2);
+  PORTA &= ~(1 << dir_4);
+  delayMicroseconds(100); // Make sure all direction set to 0, only then turn on correct direction
+  PORTA |= (1 << dir_1);
+  PORTA |= (1 << dir_3);
+  SetPWM(pwm);
+}
+
+void MoveBack(int  pwm)
 {
   PORTA &= ~(1 << dir_1);
-  PORTA |= (1 << dir_2);
   PORTA &= ~(1 << dir_3);
+  delayMicroseconds(100); // Make sure all direction set to 0, only then turn on correct direction
+  PORTA |= (1 << dir_2);
   PORTA |= (1 << dir_4);
-
-  analogWrite(wheel1, pwm);
-  analogWrite(wheel2, pwm);
-  analogWrite(wheel3, pwm);
-  analogWrite(wheel4, pwm);
+  SetPWM(pwm);
 }
 
 void Leftward(int pwm)
 {
   PORTA &= ~(1 << dir_1);
   PORTA &= ~(1 << dir_2);
+  delayMicroseconds(100); // Make sure all direction set to 0, only then turn on correct direction
   PORTA |= (1 << dir_3);
   PORTA |= (1 << dir_4);
-
-  analogWrite(wheel1, pwm);
-  analogWrite(wheel2, pwm);
-  analogWrite(wheel3, pwm);
-  analogWrite(wheel4, pwm);
-
+  SetPWM(pwm);
 }
 
-void Rightward(int pwm)
+void MoveRight(int pwm)
 {
-  PORTA |= (1 << dir_1);
-  PORTA |= (1 << dir_2);
   PORTA &= ~(1 << dir_3);
   PORTA &= ~(1 << dir_4);
-
-  analogWrite(wheel1, pwm);
-  analogWrite(wheel2, pwm);
-  analogWrite(wheel3, pwm);
-  analogWrite(wheel4, pwm);
+  delayMicroseconds(100); // Make sure all direction set to 0, only then turn on correct direction
+  PORTA |= (1 << dir_1);
+  PORTA |= (1 << dir_2);
+  SetPWM(pwm);
 }
 
 void MotorStopping()
 {
-  analogWrite(wheel1, 0);
-  analogWrite(wheel2, 0);
-  analogWrite(wheel3, 0);
-  analogWrite(wheel4, 0);
+  SetPWM(0);
 }
 
-//input process
-void dpadmovement()
+void setup() 
 {
-  if (PS4.getButtonPress(UP)) { 
-    input_signal += 1;
-    button = Up;
-    Serial.println("Forward");
-  }
-  if (PS4.getButtonPress(RIGHT)) {
-    input_signal += 2;
+  Serial.begin(115200);
 
-    Serial.println("Right");
-  }
-  if (PS4.getButtonPress(DOWN)) {
-    input_signal += 4;
-    Serial.println("Backward");
-  }
-  if (PS4.getButtonPress(LEFT)) { 
-    input_signal += 8;
-    Serial.println("Left");
-  }
-  if (PS4.getButtonPress(L1)){ 
-  }
-  if (PS4.getButtonPress(R1)){ 
-  }
-  if (PS4.getButtonPress(SQUARE)){
-  }
-  if (PS4.getButtonPress(CIRCLE)){ 
-  }
-  if (PS4.getButtonPress(TRIANGLE)){ 
-  }
-  if (PS4.getButtonPress(CROSS)){ 
-  } 
+  OperatingState = USB_Detect_Hold; // Set First State to wait user press Start button
+                                    // When Debugging, developer can point to any value of interest according to test case
+  Sync_Basic_Task();
+  PS4_Repeat_Init_Code(); // Attempt Scan for USB Host Repeatedly
+  InputTask.enable();     // Enable Input Task to be monitored
+  ProcessTask.enable();   // Enable Process Task to be monitored
+  OutputTask.enable();    // Enable Output Task to be monitored
+  DebugMessageTask.enable();  
   
 }
 
-void PS4Setup(){
-  Serial.begin(115200);
-
-  #if !defined(__MIPSEL__)
-    while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
-  #endif
-    if (Usb.Init() == -1)
-    {
-      Serial.print(F("\r\nOSC did not start"));
-      while (1); // Halt
-    }
-    Serial.print(F("\r\nPS4 Bluetooth Library Started"));
-}
-
-void setup() {
-  PS4Setup();
-}
-
-void loop() {
-  switch (state){
-    case start:
-      switch(progress){
-        case input:
-          Usb.Task();
-          if (PS4.connected()) {
-            dpadmovement();
-          }
-        case process:
-          switch (input_signal){
-            case 0:
-              MotorStopping();
-              break;
-            case 1:
-              Forward(motor_speed);
-              break;
-            case 2:
-              Backward(motor_speed);
-              break;
-            case 4:
-              Leftward(motor_speed);
-              break;
-            case 8:
-              Rightward(motor_speed);
-              break;
-            default:
-              MotorStopping();
-          }
-      }
-      case rectangle:
-        
+void loop() 
+{
+  PS4_Repeat_Init_Task.check();   // Keep Retry to check USB host connection for 1 second until connected (Refer to Variable.h)
+  if (USB_Detected)
+  {
+    InputTask.check();              // Input Task fire threads every 10ms (Refer to Variable.h)
+    ProcessTask.check();            // Process Task fire threads every 10ms (Refer to Variable.h)
+    OutputTask.check();             // Output Task fire threads every 10ms (Refer to Variable.h)
   }
+  else
+  {
+    Sync_Basic_Task();              // No USB detected, dont run anything, but keep syncing Input, Process and Output Task Interval.
+  }
+  DebugMessageTask.check();       // Debug Message Print fire threads every 1ms (Refer to Variable.h)
+}
 
+void Sync_Basic_Task()
+{
+// The following code is purposely to control when each task is being execute  
+// Without the following code, program can still run, but programmer have no idea which Task will be execute first.  
+    InputTask.reset();   // Input Task Run from 10ms from this point onwards
+    delay(3);            // Process Task Shall Execute 3ms after Input Task
+    ProcessTask.reset(); // Process Task Run from 10ms from this point onwards
+    delay(3);            // Output Task Shall Execute 3ms after Process Task
+    OutputTask.reset();  // Output Task Run from 10ms from this point onwards
+// The above code is purposely to control when each task is being execute    
 }
